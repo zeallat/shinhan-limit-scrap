9f56f63f9bffbdf1be64c5d0108e0eff9ddd3139
Jimmy Lee <jimmy@soomgo.com>
2021-10-23 14:11:26 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import {Command, flags} from '@oclif/command'
+import {
+  Command,
+  flags,
+} from '@oclif/command'
 import {scrap} from './scrap'
 import {sendTelegramMessage} from './sendTelegramMessage'
 
@@ -10,35 +13,47 @@ class ShinhanLimitScrap extends Command {
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({
+      char: 'n',
+      description: 'name to print',
+    }),
     // flag with no value (-f, --force)
     force: flags.boolean({char: 'f'}),
-    id: flags.string({char:'i',description:'신한카드 계정 아이디',required:true}),
-    password: flags.string({char:'p',description:'신한카드 계정 비밀번호',required:true}),
-    chatId: flags.string({char:'c',description:'텔레그램 채팅 아이디',required:false}),
+    id: flags.string({
+      char: 'i',
+      description: '신한카드 계정 아이디',
+      required: true,
+    }),
+    password: flags.string({
+      char: 'p',
+      description: '신한카드 계정 비밀번호',
+      required: true,
+    }),
+    chatId: flags.string({
+      char: 'c',
+      description: '텔레그램 채팅 아이디',
+      required: false,
+    }),
   }
 
   static args = [{name: 'file'}]
 
   async run() {
-    const {args, flags} = this.parse(ShinhanLimitScrap)
+    const {
+      args,
+      flags,
+    } = this.parse(ShinhanLimitScrap)
+    const result = await scrap({
+      id: flags.id,
+      password: flags.password,
+    })
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    if (flags.chatId !== undefined) {
 
-    const result = await scrap({id:flags.id, password:flags.password});
-
-
-    if(flags.chatId!==undefined){
-
-
-    await sendTelegramMessage({
-     chatId:flags.chatId,
-     message: `기간:${result?.period}\n이용:${result?.amount}\n잔여:${result?.remains}`
-    });
+      await sendTelegramMessage({
+        chatId: flags.chatId,
+        message: `기간: ${result?.period}\n이용: ${result?.amount}\n잔여: ${result?.remains}`,
+      })
     }
 
   }
